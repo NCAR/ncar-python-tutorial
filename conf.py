53337203cd52583183db200d539bcd4ae484a240
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-05-13 13:40:02 -0600

    Add nsf logo

--- conf.py
+++ conf.py
@@ -11,8 +11,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sphinx_xdev_theme
-import sphinx 
 import sys
 import os
 import datetime
@@ -49,13 +47,7 @@ extlinks = {
     
 }
 
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    extensions.append('sphinx.ext.mathjax')
-
-else:
-    extensions.append('sphinx.ext.imgmath')    
+ 
 
 current_year = datetime.datetime.now().year 
 current_month = datetime.datetime.now().month
@@ -134,7 +126,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_xdev_theme'
+html_theme = 'sphinx_rtd_theme'
+html_logo = 'assets/img/nsf.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -142,7 +135,7 @@ html_theme = 'sphinx_xdev_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_xdev_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_xdev_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
