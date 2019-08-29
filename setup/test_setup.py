b2a6eac8bb6d123b2afc0a15e0bdbe9d6fdd37b4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-08-29 00:17:05 -0600

    Update test

--- setup/test_setup.py
+++ setup/test_setup.py
@@ -1,10 +1,14 @@
 import os
 import pytest
 
-CIRCLE_CI_CHECK = os.environ.get("CIRCLECI", False)
+CIRCLE_CI_CHECK = os.environ.get("CIRCLECI")
+if CIRCLE_CI_CHECK:
+    CHECK = True
+else:
+    CHECK = False
 
 
-@pytest.mark.skipif(CIRCLE_CI_CHECK, reason="$PATH doesn't get updated properly on CIRCLI")
+@pytest.mark.skipif(CHECK, reason="$PATH doesn't get updated properly on CIRCLECI")
 def test_conda_presence():
     from distutils.spawn import find_executable
 
