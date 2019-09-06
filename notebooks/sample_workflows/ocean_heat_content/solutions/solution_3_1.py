9c81969cb4448b22028d10f2d310d100d0cddaa2
Julia Kent <jkent@ucar.edu>
2019-09-06 13:40:15 -0600

    fixed solution 3_1

--- notebooks/sample_workflows/ocean_heat_content/solutions/solution_3_1.py
+++ notebooks/sample_workflows/ocean_heat_content/solutions/solution_3_1.py
@@ -1,6 +1,6 @@
-theatao_point = ds['thetao'][0]
+thetao_point = ds['thetao'].isel(time=0, lev = 0, lat = 30, lon=30)
 
 orig_units = cf.Unit(thetao_point.attrs['units'])
 
-target_units = cf.Unit('degK)
+target_units = cf.Unit('degK')
 orig_units.convert(thetao_point, target_units)
